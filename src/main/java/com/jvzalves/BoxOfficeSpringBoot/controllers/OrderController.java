48b6c4f41f36eca6428d995122be56f2e9a2f3bf
João Vitor <jvitor145@outlook.com>
2023-12-06 21:03:16 -0300

    Implementado método POST da entidade Order

--- src/main/java/com/jvzalves/BoxOfficeSpringBoot/controllers/OrderController.java
+++ src/main/java/com/jvzalves/BoxOfficeSpringBoot/controllers/OrderController.java
@@ -3,13 +3,18 @@ package com.jvzalves.BoxOfficeSpringBoot.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jvzalves.BoxOfficeSpringBoot.DTO.OrderDTO;
+import com.jvzalves.BoxOfficeSpringBoot.entities.Order;
 import com.jvzalves.BoxOfficeSpringBoot.services.OrderService;
 
 @RestController
@@ -19,15 +24,21 @@ public class OrderController {
 	@Autowired
 	private OrderService orderService;
 
-	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public OrderDTO findById(@PathVariable Long id) {
 		OrderDTO result = orderService.findById(id);
 		return result;
 	}
 
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<OrderDTO> findAll() {
 		List<OrderDTO> result = orderService.findAll();
 		return result;
+	}
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<OrderDTO> updateOrder(@RequestBody Order order) {
+		   orderService.updateOrder(order);
+		   return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 }
--- src/main/java/com/jvzalves/BoxOfficeSpringBoot/services/OrderService.java
+++ src/main/java/com/jvzalves/BoxOfficeSpringBoot/services/OrderService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.jvzalves.BoxOfficeSpringBoot.DTO.OrderDTO;
 import com.jvzalves.BoxOfficeSpringBoot.entities.Order;
@@ -20,13 +21,13 @@ public class OrderService {
 	@Transactional(readOnly = true)
 	public OrderDTO findById(Long id) {
 		try {
-			 Order result = orderRepository.findById(id).get();
-			 OrderDTO dto = new OrderDTO(result);
-			 return dto;
+			Order result = orderRepository.findById(id).get();
+			OrderDTO dto = new OrderDTO(result);
+			return dto;
 		} catch (Exception e) {
 			throw new OrderIdNotFoundException("Enter a correct id");
 		}
-	}	
+	}
 
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
@@ -34,5 +35,15 @@ public class OrderService {
 		List<OrderDTO> dto = result.stream().map(x -> new OrderDTO(x)).toList();
 		return dto;
 	}
-
+	
+    @Transactional
+	public OrderDTO updateOrder(@RequestBody Order order) {
+		try {
+			Order result = orderRepository.save(order);
+			OrderDTO dto = new OrderDTO(result);
+			return dto;
+		} catch (Exception e) {
+			throw new OrderIdNotFoundException("Error creating order");
+		}
+	}
 }
