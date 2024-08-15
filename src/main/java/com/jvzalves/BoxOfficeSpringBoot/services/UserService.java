6c337c0fb05f1bc0c4d991ad83ebd9159b3a5834
João Vitor <jvitor145@outlook.com>
2024-08-15 20:43:44 -0300

    Implementando o Repository e Service

---
+++ src/main/java/com/jvzalves/BoxOfficeSpringBoot/services/UserService.java
@@ -0,0 +1,32 @@
+package com.jvzalves.BoxOfficeSpringBoot.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.jvzalves.BoxOfficeSpringBoot.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+
+	@Autowired
+    UserRepository userRepository;
+	
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		var user = userRepository.findByUserName(username);
+		if (user != null) {
+			return user;
+		} else {
+
+			throw new UsernameNotFoundException("Username " + username + " not found");
+		}
+	}
+}
